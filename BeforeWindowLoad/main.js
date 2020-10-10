bcc0bb78278c217ad2ce080886e197cf8edaa5dd
kaixing2012 <59598698+kaixing2012@users.noreply.github.com>
2020-10-10 09:00:57 +0800

    init before window load

---
+++ BeforeWindowLoad/main.js
@@ -0,0 +1,24 @@
+// .animate(
+//     [
+//         {
+//             height: '0',
+//             width: '0',
+//         },
+//         {
+//             height: '0%',
+//             width: '100%',
+//         },
+//         {
+//             height: '100%',
+//             maxHeight: '320px',
+//             width: '100%',
+//         }
+//     ],
+//     {
+//         duration: 1000,
+//         fill: `forwards`,
+//     }
+// )
+
+
+
